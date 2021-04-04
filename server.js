1bf76a89bbf7aa4c48a2aa288839815d6a038577
robertsicher <62100753+robertsicher@users.noreply.github.com>
2021-04-04 15:15:12 +0100

    Dotenv

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require('./routes');
 const mongoose = require("mongoose");
+require('dotenv').config()
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
